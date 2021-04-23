124c11f9a0dad97387ba94c7851b9e04162bd165
☸️ <29265684+buffermet@users.noreply.github.com>
2021-04-23 23:34:53 +1000

    Create binance-multichart-intervals.js

---
+++ binance-multichart-intervals.js
@@ -0,0 +1,34 @@
+/**
+ * Automatically change kline intervals of multiple Binance charts by holding ALT key.
+ */
+
+globalThis.addEventListener("load", () => {
+  if (location.pathname.endsWith("multipleChart")) {
+    let altPressed = false;
+
+    globalThis.addEventListener("keydown", event => {
+      if (event.altKey) altPressed = true;
+    });
+
+    globalThis.addEventListener("keyup", event => {
+      if (!event.altKey) altPressed = false;
+    });
+
+    const intervalButtons = document.querySelectorAll(
+      "div.css-e2pgpg div.css-137m6a4," +
+      "div.css-e2pgpg div.css-137m6a4," + 
+      "div.css-e2pgpg div.css-14effk1");
+
+    intervalButtons.forEach(button => {
+      button.addEventListener("mousedown", event => {
+        if (altPressed) {
+          intervalButtons.forEach(_button => {
+            if (_button.id === event.target.id) {
+              _button.click();
+            }
+          });
+        }
+      });
+    });
+  }
+});
